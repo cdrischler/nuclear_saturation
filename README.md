1117d4fda30b887a5f94ecf95aa3ef01d3561217
Christian Drischler <drischler@frib.msu.edu>
2022-05-31 11:49:52 -0400

    adds a simple Jupyter notebook to plot Skyrme and RMF model constraints on the empirical point

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# nuclear_saturation
-Study of the empirical saturation point in symmetric nuclear matter
+# Nuclear saturation point
+
+We use Bayesian model mixing to quantify the uncertainties in the empirical nuclear saturation point of symmetric matter. We also quantity how well recent microscopic calculations reproduce the empirical point.
+
+## Installation
+
+```shell
+python3 -m venv env_satpoint
+source env_satpoint/bin/activate
+pip3 install -r requirements.txt
+jupyter-notebook&
+# deactivate # when the job is done
+```
+
+## References
+
+* [Bayesian analysis][McDonnell:2015] of Skyrme functionals, which was updated [here][Schunck:2020].
+* [Empirical saturation point][Drischler:2016] (see Section IV.B) used in microscopic calculations based on:
+  * Table VII in [Dutra et al. (2012)][Dutra:2012]
+  * Table I in [Brown & Schwenk][Brown:2013]
+  * Table IV in [Kortelainen et al.][Kortelainen:2014]
+* Analysis of RMF models by [Dutra et al. (2014)][Dutra:2014] could be used here as well.
+* Lattimer's talk at the [INT 2022][LattimerINT:2022] (see slide 10)
+* [Insights into nuclear saturation density from parity violating electron scattering][Horowitz:2020]
+* [From finite nuclei to the nuclear liquid drop: Leptodermous expansion based on self-consistent mean-field theory][Reinhard:2005]
+* [Nuclear charge and neutron radii and nuclear matter: trend analysis][Reinhard:2016]
+
+[McDonnell:2015]:https://arxiv.org/abs/1501.03572
+[Schunck:2020]:https://arxiv.org/abs/2003.12207
+[Drischler:2016]:https://arxiv.org/abs/1510.06728
+[Brown:2013]:https://arxiv.org/abs/1311.3957
+[Dutra:2014]:https://arxiv.org/abs/1405.3633
+[Dutra:2012]:https://arxiv.org/abs/1202.3902
+[Kortelainen:2014]:https://arxiv.org/abs/1312.1746
+[LattimerINT:2022]:https://www.int.washington.edu/sites/default/files/schedule_session_files/Lattimer%2C%20J.pdf
+
+[Horowitz:2020]:https://arxiv.org/abs/2007.07117
+[Reinhard:2005]:https://arxiv.org/abs/nucl-th/0510039
+[Reinhard:2016]:https://arxiv.org/abs/1601.06324
---
+++ plot_helpers.py
@@ -0,0 +1,172 @@
+import numpy as np
+import matplotlib as mpl
+from matplotlib.patches import Ellipse
+import matplotlib.transforms as transforms
+import matplotlib.pyplot as plt
+
+
+cm=1./2.54
+
+fontsize = 9
+mpl.rcParams['font.size'] = fontsize
+mpl.rcParams['axes.labelsize'] = fontsize
+mpl.rcParams['axes.titlesize'] = fontsize
+mpl.rcParams['xtick.labelsize'] = fontsize
+mpl.rcParams['ytick.labelsize'] = fontsize
+mpl.rcParams['legend.title_fontsize'] = fontsize
+mpl.rcParams['legend.fontsize'] = fontsize
+mpl.rcParams['figure.titlesize'] = fontsize
+mpl.rcParams['text.usetex'] = True
+mpl.rcParams['figure.dpi'] = 130
+mpl.rcParams['font.family'] = 'serif'
+mpl.rcParams['ytick.direction'] = 'in'
+mpl.rcParams['xtick.direction'] = 'in'
+plt.rcParams["xtick.minor.visible"] =  True
+plt.rcParams["ytick.minor.visible"] =  True
+plt.rcParams['ytick.right'] = True
+plt.rcParams['xtick.top'] = True
+mpl.rc('savefig', transparent=False, bbox='tight', pad_inches=0.05, format='pdf')
+
+color_68 = 'darkgrey'   # color for 1 sigma bands
+color_95 = 'lightgrey'  # color for 2 sigma bands
+colors = ['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33', '#a65628', '#f781bf']  # 8 colors
+colorset = ['b', 'r', 'c', 'darkslateblue', 'orange', 'darkcyan', 'darkgrey', 'lime', 'aqua', 'g', 'magenta', 'k']
+markers = {'0': "*", '2': "o", '3': "s", '4': "D", '5': "p" }
+
+
+def confidence_ellipse_mean_cov(mean, cov, ax, n_std=3.0, facecolor='none', **kwargs):
+    """
+    Create a plot of the covariance confidence ellipse of *x* and *y*.
+
+    Parameters
+    ----------
+    x, y : array-like, shape (n, )
+        Input data.
+    ax : matplotlib.axes.Axes
+        The axes object to draw the ellipse into.
+    n_std : float
+        The number of standard deviations to determine the ellipse's radiuses.
+
+    Returns
+    -------
+    matplotlib.patches.Ellipse
+
+    Other parameters
+    ----------------
+    kwargs : `~matplotlib.patches.Patch` properties
+    """
+    pearson = cov[0, 1] / np.sqrt(cov[0, 0] * cov[1, 1])
+    # Using a special case to obtain the eigenvalues of this
+    # two-dimensionl dataset.
+    ell_radius_x = np.sqrt(1 + pearson)
+    ell_radius_y = np.sqrt(1 - pearson)
+    ellipse = Ellipse((0, 0),
+        width=ell_radius_x * 2,
+        height=ell_radius_y * 2,
+        facecolor=facecolor,
+        **kwargs)
+
+    # Calculating the standard deviation of x from
+    # the square root of the variance and multiplying
+    # with the given number of standard deviations.
+    scale_x = np.sqrt(cov[0, 0]) * n_std
+    # calculating the stdandard deviation of y ...
+    scale_y = np.sqrt(cov[1, 1]) * n_std
+
+    transf = transforms.Affine2D() \
+        .rotate_deg(45) \
+        .scale(scale_x, scale_y) \
+        .translate(mean[0], mean[1])
+
+    ellipse.set_transform(transf + ax.transData)
+    return ax.add_patch(ellipse)
+
+def confidence_ellipse(x, y, ax, n_std=3.0, facecolor='none', show_scatter=False, **kwargs):
+    """
+    Create a plot of the covariance confidence ellipse of *x* and *y*.
+
+    Parameters
+    ----------
+    x, y : array-like, shape (n, )
+        Input data.
+    ax : matplotlib.axes.Axes
+        The axes object to draw the ellipse into.
+
+    n_std : float
+        The number of standard deviations to determine the ellipse's radiuses.
+
+    Returns
+    -------
+    matplotlib.patches.Ellipse
+
+    Other parameters
+    ----------------
+    kwargs : `~matplotlib.patches.Patch` properties
+    """
+
+    if x.size != y.size:
+        raise ValueError("x and y must be the same size")
+
+    mean_x = np.mean(x)
+    mean_y = np.mean(y)
+    mean = np.array([mean_x, mean_y])
+    cov = np.cov(x, y)
+    #print(mean)
+    #print(cov)
+
+    if show_scatter:
+        scat_color = darken_color(facecolor, 0.5)
+        ax.plot(x, y, ls='', marker='.', markersize=0.6, color=scat_color)
+
+    return confidence_ellipse_mean_cov(mean, cov, ax, n_std=n_std, facecolor=facecolor, **kwargs)
+
+def lighten_color(color, amount=0.5):
+    """
+    Lightens the given color by multiplying (1-luminosity) by the given amount.
+    Input can be matplotlib color string, hex string, or RGB tuple.
+
+    Examples:
+    >> lighten_color('g', 0.3)
+    >> lighten_color('#F034A3', 0.6)
+    >> lighten_color((.3,.55,.1), 0.5)
+    """
+    import matplotlib.colors as mc
+    import colorsys
+    try:
+        c = mc.cnames[color]
+    except:
+        c = color
+    c = colorsys.rgb_to_hls(*mc.to_rgb(c))
+    return colorsys.hls_to_rgb(c[0], 1 - amount * (1 - c[1]), c[2])
+
+def darken_color(color, amount=0.5):
+    """
+    Darken the given color by multiplying (1-luminosity) by the given amount.
+    Input can be matplotlib color string, hex string, or RGB tuple.
+
+    Examples:
+    >> darken_color('g', 0.3)
+    >> darken_color('#F034A3', 0.6)
+    >> darken_color((.3,.55,.1), 0.5)
+    """
+    return lighten_color(color, 1./amount)
+
+def plot_empirical_saturation(ax=None, facecolor='lightgray', edgecolor='gray', alpha=0.4, zorder=9, **kwargs):
+    if ax is None:
+        ax = plt.gca()
+    from matplotlib.patches import Rectangle
+    # From Drischler et al. (2017), arXiv:1710.08220
+    n0 = 0.164 # fm**-3
+    n0_std = 0.007 # fm**-3
+    y0 = -15.9 # MeV
+    y0_std = 0.4  # MeV; errors are added linearly
+    # y0_std = np.sqrt(0.37 ** 2 + 0.2 ** 2) # MeV; use this to add them in quadrature
+    left = n0 - n0_std
+    right = n0 + n0_std
+    rect = Rectangle(
+        (left, y0 - y0_std), width=right - left, height=2 * y0_std,
+        facecolor=facecolor, edgecolor=edgecolor, alpha=alpha, zorder=zorder, **kwargs
+    )
+    ax.add_patch(rect)
+    return ax
+#%%
