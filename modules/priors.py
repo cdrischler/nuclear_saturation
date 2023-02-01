72b29f5e520d1686afdfd06e35d0716b77db53ef
Christian Drischler <drischler@frib.msu.edu>
2023-02-01 10:18:12 -0500

    simple bugfix

--- modules/priors.py
+++ modules/priors.py
@@ -21,7 +21,7 @@ setB_prior_params = {"mu": np.array([0.16, -15.9]),
 setC_prior_params = {"mu": np.array([0.16, -15.9]),
                      "Psi": np.array([[0.006**2, standard_prior_off_diag],
                                       [standard_prior_off_diag, 0.62**2]]),
-                     "kappa": 3, "nu": 3, "label": "Set C"}gi
+                     "kappa": 3, "nu": 3, "label": "Set C"}
 
 used_prior_sets = [setA_prior_params, setB_prior_params, setC_prior_params]
 
