139ca27b33769cd2f738bd2dfdc0cb8770e4a9e2
Christian Drischler <drischler@frib.msu.edu>
2023-02-15 16:02:47 -0500

    redefines prior sets a and b

--- modules/priors.py
+++ modules/priors.py
@@ -7,16 +7,15 @@ standard_prior_params = {"mu": np.array([0.16, -15.9]),
                                           [standard_prior_off_diag, 0.32**2]]),
                          "kappa": 1, "nu": 10, "label": "Standard Prior"}
 
-
 setA_prior_params = {"mu": np.array([0.16, -15.9]),
-                     "Psi": np.array([[0.008**2, standard_prior_off_diag],
+                     "Psi": np.array([[0.01**2, standard_prior_off_diag],
                                       [standard_prior_off_diag, 0.32**2]]),
-                     "kappa": 3, "nu": 3, "label": "Set A"}
+                     "kappa": 1, "nu": 10, "label": "Set A"}
 
 setB_prior_params = {"mu": np.array([0.16, -15.9]),
-                     "Psi": np.array([[0.006**2, standard_prior_off_diag],
-                                      [standard_prior_off_diag, 0.62**2]]),
-                     "kappa": 3, "nu": 3, "label": "Set B"}
+                     "Psi": np.array([[0.01**2, standard_prior_off_diag],
+                                      [standard_prior_off_diag, 0.32**2]]),
+                     "kappa": 1, "nu": 4, "label": "Set B"}
 
 setC_prior_params = {"mu": np.array([0.16, -15.9]),
                      "Psi": np.array([[0.006**2, standard_prior_off_diag],
