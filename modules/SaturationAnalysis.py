167bdce5a142d585d52519867ef9d4c0ec4fae31
Christian Drischler <drischler@frib.msu.edu>
2024-04-08 17:10:55 -0400

    avoids loading package pymc3, which causes problem on current platforms

--- modules/SaturationAnalysis.py
+++ modules/SaturationAnalysis.py
@@ -7,7 +7,6 @@ import numpy as np
 import os
 import arviz as az
 from modules.DataSets import GenericDataSet, NormDistDataSet, KernelDensityEstimate
-from modules.EftPredictions import *
 from modules.StatisticalModel import StatisticalModel, multivariate_t, multivariate_normal, standard_prior_params
 import matplotlib.backends.backend_pdf
 from modules.DataSets import Scenario
@@ -57,7 +56,10 @@ class SaturationAnalysis:
             if not os.path.exists(path):
                 os.mkdir(path)
         self.drischler_satbox = drischler_satbox
-        self.eft_predictions = EftPredictions(show_result=True) if prestore_eft_fit else None
+        self.eft_predictions = None
+        if prestore_eft_fit:
+            from modules.EftPredictions import EftPredictions
+            self.eft_predictions = EftPredictions(show_result=True)
 
     def plot_constraints(self, dft_constraints=None, eft=False, dft_conf_level=0.95,
                          eft_conf_level=0.95, eft_plot_scatter=True, add_svm=True):
